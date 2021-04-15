909ec880407acc0ff7ba9b8369b041822e5637d1
Yeseo Lee <s2019w35@e-mirim.hs.kr>
2021-04-15 17:16:36 +0900

    main login popup update

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,31 @@
-const toggleMenu = document.querySelector('.toggle-menu');
-const menu = document.querySelector('.menu');
+const toggleMenu = document.querySelector(".toggle-menu");
+const menu = document.querySelector(".menu");
 
 const clickMenu = () => {
-  menu.classList.toggle('active');
-  snsButtons.classList.toggle('active');
+  menu.classList.toggle("active");
+  snsButtons.classList.toggle("active");
 };
 
-toggleMenu.addEventListener('click', clickMenu);
+toggleMenu.addEventListener("click", clickMenu);
+
+
+// 로그인 팝업
+
+$('a.popper').hover(function (e) {    
+  var target = '#' + ($(this).attr('data-popbox'));
+  $(target).show();
+  moveLeft = $(this).outerWidth();
+  moveDown = ($(target).outerHeight() / 2);
+}, function () {
+  var target = '#' + ($(this).attr('data-popbox'));
+  if (!($("a.popper").hasClass("show"))) {
+      $(target).hide();
+  }
+});
+$('a.popper').click(function (e) {
+  var target = '#' + ($(this).attr('data-popbox'));
+  if (!($(this).hasClass("show"))) {
+      $(target).show();
+  }
+  $(this).toggleClass("show");
+});

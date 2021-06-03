17cb83b499dbec039e40ef5f78639e6e1311a350
6h15m <s2019w35@e-mirim.hs.kr>
2021-06-03 16:11:42 +0900

    main template update

--- js/nav.js
+++ js/nav.js
@@ -8,24 +8,24 @@ const clickMenu = () => {
 
 toggleMenu.addEventListener("click", clickMenu);
 
-
-// 로그인 팝업
-
-$('a.popper').hover(function (e) {    
-  var target = '#' + ($(this).attr('data-popbox'));
-  $(target).show();
-  moveLeft = $(this).outerWidth();
-  moveDown = ($(target).outerHeight() / 2);
-}, function () {
-  var target = '#' + ($(this).attr('data-popbox'));
-  if (!($("a.popper").hasClass("show"))) {
+$("a.popper").hover(
+  function (e) {
+    var target = "#" + $(this).attr("data-popbox");
+    $(target).show();
+    moveLeft = $(this).outerWidth();
+    moveDown = $(target).outerHeight() / 2;
+  },
+  function () {
+    var target = "#" + $(this).attr("data-popbox");
+    if (!$("a.popper").hasClass("show")) {
       $(target).hide();
+    }
   }
-});
-$('a.popper').click(function (e) {
-  var target = '#' + ($(this).attr('data-popbox'));
-  if (!($(this).hasClass("show"))) {
-      $(target).show();
+);
+$("a.popper").click(function (e) {
+  var target = "#" + $(this).attr("data-popbox");
+  if (!$(this).hasClass("show")) {
+    $(target).show();
   }
   $(this).toggleClass("show");
 });

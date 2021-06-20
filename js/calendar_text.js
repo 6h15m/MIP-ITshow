444b433fdb8171d6899f322de3ba88e2f6c0f5cb
username <user@email.mail>
2021-06-21 04:59:08 +0900

    갤러리 메인 화면 틀&연결

---
+++ js/calendar_text.js
@@ -0,0 +1,8 @@
+function btn1() {
+    document.getElementById("btn1").innerHTML="전해원";
+    document.getElementById("img1").src="img/color_1.jpg"
+}
+
+function btn2() {
+    document.getElementById("btn2").innerHTML="성진민";
+}
